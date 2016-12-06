c2fd13f5f0a7ea70bb4bda6555bec6dc74fc61ea
albertob <alberto.bonora@gmail.com>
2016-12-05 22:23:19 -0500

    footer and loop article changes
    added triangle overlays to main image and footer styles

--- loop.php
+++ loop.php
@@ -1,11 +1,11 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 	<!-- article -->
-	<div class="col-xs-12 col-sm-6 col-md-4" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="col-xs-12 col-sm-6 col-md-4 article-loop" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<!-- post thumbnail -->
 		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" class="post-thumb">
 				<?php the_post_thumbnail(large); // Declare pixel size you need inside the array ?>
 			</a>
 		<?php endif; ?>
